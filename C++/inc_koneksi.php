457f0fd06e63062721e738c60458a14e34bf1644
kikiqball <91469822+kikiqball@users.noreply.github.com>
2021-12-22 13:51:15 +0700

    Add files via upload

---
+++ C++/inc_koneksi.php
@@ -0,0 +1,10 @@
+<?php
+$host     = "localhost";
+$user     = "root";
+$pass     = "";
+$db       = "userprofile"
+
+#koneksi  = mysqli_connect(#host,#user,$pass,$db);
+if(!$konkesi){
+    die("gagal terkoneksi");
+}
